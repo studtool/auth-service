eee53601d6f3f880eb64dbf0e5b6ddc8690f5e5d
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-07-24 21:53:15 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/studtool/auth-service
+
+go 1.12
---
+++ install_tools.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+dir="$1"
+
+if ! which "${dir}/easyjson" &> /dev/null ; then
+    GOBIN="${dir}" go get github.com/mailru/easyjson/easyjson && go mod tidy
+fi
+if ! which "${dir}/golangci-lint" &> /dev/null ; then
+    GOBIN="${dir}" go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.17.1 && go mod tidy
+fi
